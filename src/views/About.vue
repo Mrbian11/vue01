5a99c5b3a08cfc6a68142287c4f1baccf30d7371
bianbian <johndoe@1740934786qq.com>
2020-12-07 16:14:53 +0800

    vue

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1 @click="onclick">This is an about page</h1>
+  </div>
+</template>
+<script>
+  export default {
+    props:['age'],
+    methods:{
+      onclick(){
+        this.$emit('xxx',{
+          username:"边晨旭"
+        })
+      }
+    }
+  }
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1 :class="{xxx:classed,add:!classed}">111</h1>
+    <div @click="onclick">wode</div>
+    <div @click="onclick2('111',$event)">123</div>
+    <a 
+      href="http://www.so.com"
+      target="_blank"
+      @click.passive="onclick3"></a>
+      <el-button @click="onClick">点我</el-button>
+      <input v-model="age"/>
+      <h1>{{age}}</h1>
+      <About :age="age" @xxx="onABc"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import About from './About'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  data(){
+    return{
+      age:"小花"
+    }
+  },
+  methods:{
+    onClick(){
+      this.age = new Date().getTime()
+    },
+    onABc(opt){
+      console.log(opt,1)
+    }
+  },
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
